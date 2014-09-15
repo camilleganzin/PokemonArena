ab82c0ea2806f57b5c7df947d3789c8233a27c20
Camille <camilleganzin@gmail.com>
2014-09-15 11:44:50 +0200

    ajout d'une page pokemon.html

--- index.php
+++ index.php
@@ -9,6 +9,14 @@ function index() {
 	return array('nom' => 'PokemonArena');
 }
 
+/**
+*@route /pokemon/:name
+*@view /views/pokemon.html
+*/
+function pokemon($name) {
+	return array('pokemon' => $name);
+}
+
 define('APPLICATION_PATH', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 
 require_once('bottle/bottle.php');
